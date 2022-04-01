735b33ff62f193ab97f688a942015a573f979d61
CesarCouchinho <cesar.couchinho@gmail.com>
2022-04-01 23:39:59 +0100

    start of create_auth_file

--- bank.py
+++ bank.py
@@ -14,15 +14,15 @@ def create_parser() -> argparse.ArgumentParser:
     usage = '-p <port> -s <auth-file>'
     parser = argparse.ArgumentParser(usage=usage, description=description, exit_on_error=False)
 
-    parser.add_argument('-p', type=int, metavar='<Port>', help='The port to listen on', default=3000)
-    parser.add_argument('-s', metavar='<auth-file>', help='Name of the auth-file')
+    parser.add_argument('-p', type=int, metavar='<Port>', dest='port', help='The port to listen on', default=3000)
+    parser.add_argument('-s', type=str, metavar='<auth-file>', dest='filename', help='Name of the auth-file')
 
     return parser
 
 
 def server(args):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = args.p
+    port = args.port
     s.bind(('', port))
     s.listen()
     print("listening on port ", port)
@@ -37,6 +37,16 @@ def server(args):
             print(data.decode('utf-8'))
 
 
+def create_auth_file(filename: str):
+    try:
+        f = open(filename, "x")
+        f.write(filename)
+        f.close()
+        print("created")
+    except Exception:
+        proper_exit('File exists')
+
+
 def main() -> None:
     if is_admin():
         proper_exit('Error: the script must run as unprivileged/regular user')
@@ -48,12 +58,13 @@ def main() -> None:
         proper_exit('', 0)
 
     args = parser.parse_args()
-
+    create_auth_file(args.filename)
     server(args)
 
 
 if __name__ == '__main__':
     try:
         main()
-    except Exception:
+    except Exception as err:
+        print(err)
         proper_exit('Program encountered an error during execution and cannot continue. Contact the support team')
