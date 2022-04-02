653c481044a3e3cd9b77557d8d98c50607017010
CesarCouchinho <cesar.couchinho@gmail.com>
2022-04-02 22:07:03 +0100

    update to atm.py nº2

--- atm.py
+++ atm.py
@@ -18,8 +18,12 @@ def run_atm(args):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         #TO DO
-        data='{"account": "john"}'
-        s.sendall(data.encode())
+        m = {"account": args.account, "auth_file": args.auth_file, "ip_address": args.ip_address, "port": args.port,
+             "card_file": args.card_file, "balance": args.balance, "deposit_amount": args.deposit_amount,
+             "withdraw_amount": args.withdraw_amount, "get": args.get}
+        data = json.dumps(m)
+
+        s.sendall(bytes(data,encoding="utf-8"))
         data = s.recv(1024)
 
         print(f"Received {data!r}")
@@ -28,6 +32,13 @@ def run_atm(args):
 def validate_args(args) -> Response:
     #  if len(args.filename) > 1 | len(args.port) > 1:
     #     return Response(False, 'Arguments cannot be duplicate')
+
+    if not args.account:
+        return Response(False, 'argument -a must be provided')
+
+    if not args.balance and not args.deposit_amount and not args.withdraw_amount and not args.get:
+        return Response(False, 'a mode of operations must be provided (-n | -d | -w | -g)')
+
     print(args)
     return Response(True, args)
 
@@ -38,17 +49,17 @@ def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(usage=usage, description=description, exit_on_error=False)
     commands = parser.add_argument_group('available commands')
 
-    commands.add_argument('-a', metavar='account', type=str, dest='account', help='')
-    commands.add_argument('-s', metavar='auth_file', type=str, dest='auth_file', help='')
-    commands.add_argument('-i', metavar='ip_address', type=str, dest='ip_address', help='', default='127.0.0.1')
-    commands.add_argument('-p', metavar='port', type=int, dest='port', help='', default=3000)
-    commands.add_argument('-c', metavar='card_file', type=path_type, dest='card_file', help='')
+    commands.add_argument('-a', metavar='account', type=str, dest='account', help='Name of the account')
+    commands.add_argument('-s', metavar='auth_file', type=str, dest='auth_file', help='Name of the auth-file')
+    commands.add_argument('-i', metavar='ip_address', type=str, dest='ip_address', help='IP address to connect to', default='127.0.0.1')
+    commands.add_argument('-p', metavar='port', type=int, dest='port', help='The port the bank is listening on', default=3000)
+    commands.add_argument('-c', metavar='card_file', type=path_type, dest='card_file', help='The customer card file')
 
     mode = commands.add_mutually_exclusive_group()
-    mode.add_argument('-n', metavar='balance', type=int, dest='balance', help='')
-    mode.add_argument('-d', metavar='deposit_amount', type=int, dest='deposit_amount', help='')
-    mode.add_argument('-w', metavar='withdraw_amount', type=int, dest='withdraw_amount', help='')
-    mode.add_argument('-g', action='store_const', const='get', dest='get', help='get balance')
+    mode.add_argument('-n', metavar='balance', type=int, dest='balance', help='Create a new account with given balance')
+    mode.add_argument('-d', metavar='deposit_amount', type=int, dest='deposit_amount', help='Amount to deposit')
+    mode.add_argument('-w', metavar='withdraw_amount', type=int, dest='withdraw_amount', help='Amount to withdraw')
+    mode.add_argument('-g', action='store_const', const='get', dest='get', help='Get balance')
 
     return parser
 
@@ -59,7 +70,7 @@ def main() -> None:
 
     parser = create_parser()
 
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         parser.print_help()
         proper_exit('', 0)
 
